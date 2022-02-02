32dd689661cbdc043dfa13d7f70d74d57a5b2cce
M3T4Z0D <lucas.cabral.cg18@gmail.com>
2022-02-02 11:33:08 -0400

    Criação de uma classe de conexão com a API

---
+++ public/js/apiConnect.js
@@ -0,0 +1,17 @@
+// THE COCKTAIL DB https://thecocktaildb.com/api.php
+
+class Connections {
+    static async getRandomDrink(){
+        let response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
+        let data = await response.json();
+    
+        return data.drinks[0];
+    }
+
+    static async getDrinksByName(name){
+        let response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        let data = await response.json();
+
+        return data.drinks;
+    }
+}
