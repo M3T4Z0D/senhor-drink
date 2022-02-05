7f0780facbc531a377e85f69d7d5213c0127d023
M3T4Z0D <lucas.cabral.cg18@gmail.com>
2022-02-05 10:49:54 -0400

    Criação dos cards com os drinks

--- public/js/frontCodes.js
+++ public/js/frontCodes.js
@@ -1,10 +1,35 @@
 class FrontCodes {
     static textRandom = 'Clique para gerar um drink aleatório';
-    static pathLoadingGif = '../public/images/loading.gif';
+    static URL_ROOT = location.href.substring(0, location.href.lastIndexOf('/') + 1);
+    static pathLoadingGif = `${this.URL_ROOT}/public/images/loading.gif`;
 
     static listaItens(itens){
         let ul = document.getElementById('listaDrink');
         itens.forEach((item) => {
+            let card = document.createElement('div');
+            card.setAttribute('class', 'card');
+
+            let cardHeader = document.createElement('div');
+            cardHeader.setAttribute('class', 'card-header');
+            cardHeader.appendChild(document.createTextNode(item.strDrink));
+
+            let cardBody = document.createElement('div');
+            cardBody.setAttribute('class', 'card-body');
+            let img = document.createElement('img');
+            img.setAttribute('src', item.strDrinkThumb);
+            cardBody.appendChild(img);
+
+            let cardFooter = document.createElement('div');
+            cardFooter.setAttribute('class', 'card-footer');
+            cardFooter.appendChild(document.createTextNode(item.strInstructions))
+
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+            card.appendChild(cardFooter);
+
+            let carousel = document.getElementsByClassName('drinksList')[0].children[0];
+            carousel.appendChild(card);
+
             let link = document.createElement('a');
             let li = document.createElement('li');
             li.setAttribute('id', item.idDrink);
